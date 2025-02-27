d6eaca534c15aa5de2538a5c36b286c33dd9ea14
thatgato <thatgatooo@gmail.com>
2025-02-27 20:33:23 +0100

    Backend for vector operations

--- src/classes/cmds/cogeo/vectors/CVecLen.cpp
+++ src/classes/cmds/cogeo/vectors/CVecLen.cpp
@@ -38,53 +38,60 @@ CVecLen::CVecLen()
 
 /* -------------------------------- Internal -------------------------------- */
 
-vec<double> getCoords(vec<str>& splitStr) {
-    vec<double> coords;
+vec<double> getCoords(vec<str> &splitStr) {
+  vec<double> coords;
 
-    for (auto& element : splitStr) {
-        std::pair<bool, double> res = Utils::TryConvertStrToDouble(element);
-        if (!res.first) {
-            Logger.Log(
-                "Failed to convert one of the coordinates into a double!",
-                LogLevel::WARN);
-            Logger.In();
-            coords.clear();
-            return coords;
-        }
-        coords.push_back(res.second);
+  for (auto &element : splitStr) {
+    std::pair<bool, double> res = Utils::TryConvertStrToDouble(element);
+    if (!res.first) {
+      Logger.Log("Failed to convert one of the coordinates into a double!",
+                 LogLevel::WARN);
+      Logger.In();
+      coords.clear();
+      return coords;
     }
-    return coords;
+    coords.push_back(res.second);
+  }
+  return coords;
 }
 
 /* -------------------------------- external -------------------------------- */
 
 ECommandResult CVecLen::Run(bool isFirst) {
-    if (isFirst) {
-        system("cls");  // purge!
+  if (isFirst) {
+    system("cls"); // purge!
 
-        Utils::DisplayCmdDataOnRun(*this);
-    }
+    Utils::DisplayCmdDataOnRun(*this);
+  }
 
-    Logger.Out("Gib me vector!");
+  Logger.Out("Gib me vector!");
 
-    str inp = Logger.In();
+  str inp = Logger.In();
 
-    // Internals
-    EPostCmdAction res = Core::GET_INTERNAL_CMD_RESULT(inp);
-    if (res == EXIT_CMD_CTX) {
-        return SUCCESS;
-    }
-
-    // TODO: prob move this to utils
-    vec<str> splitStr = Utils::SplitStrByDelimiter(inp, ' ');
-
-    vec<double> coords = getCoords(splitStr);
-    if (coords.empty()) this->Run(false);
-
-    NVector VecClass(coords);
-    Logger.Out(std::format(
-        "The vector's length is: {}{}{}\n\n", SStyle::green + SStyle::italic,
-        Utils::RemoveTrailingZeroesFromNum(VecClass.Length()), SStyle::reset));
-    this->Run(false);
+  // Internals
+  EPostCmdAction res = Core::GET_INTERNAL_CMD_RESULT(inp);
+  if (res == EXIT_CMD_CTX) {
     return SUCCESS;
+  }
+
+  // TODO: prob move this to utils
+  vec<str> splitStr = Utils::SplitStrByDelimiter(inp, ' ');
+
+  vec<double> coords = getCoords(splitStr);
+  if (coords.empty())
+    this->Run(false);
+
+  NVector VecClass(coords);
+
+  vec<float> a{11, 2, 3};
+  vec<float> b{2, 5, 4};
+  NVector A(a);
+  NVector B(b);
+  Logger.Out("Btw the lerp of the two vectors is " + A.Lerp(B, 0.5).toString());
+
+  Logger.Out(std::format(
+      "The vector's length is: {}{}{}\n\n", SStyle::green + SStyle::italic,
+      Utils::RemoveTrailingZeroesFromNum(VecClass.Length()), SStyle::reset));
+  this->Run(false);
+  return SUCCESS;
 }
--- src/include/mathlib/vectors.h
+++ src/include/mathlib/vectors.h
@@ -17,24 +17,71 @@
 #pragma once
 
 #include "types/general.h"
+#include <stdexcept>
+#include <string>
 
 template <typename T>
 
 class NVector {
-   private:
-    vec<T> m_coords;
+private:
+  vec<T> m_coords;
 
-   public:
-    NVector(vec<T>& coords) : m_coords(coords) {}
+public:
+  NVector(vec<T> &coords) : m_coords(coords) {}
 
-    vec<T>& GetCoords() { return m_coords; }
+  vec<T> &GetCoords() { return m_coords; }
 
-    // Math
-    T Length() {
-        T sum = 0;
-        for (T coord : m_coords) {
-            sum += pow(coord, 2);
-        }
-        return sqrtf(sum);
-    };
+  // Math
+  T Length() {
+    T sum = 0;
+    for (T coord : m_coords) {
+      sum += pow(coord, 2);
+    }
+    return sqrtf(sum);
+  };
+
+  int ComponentLength() { return m_coords; }
+  NVector<T> Lerp(NVector<T> &other, float alpha) {
+    return (1 - alpha) * (*this) + alpha * other;
+  }
+
+  // Ok so GPT came in clutch with these, i dont fully understand the friend
+  // keyword, research later pls
+
+  friend NVector operator*(float scalar, const NVector &vec) {
+    std::vector<T> multiplied;
+    for (const auto &val : vec.m_coords) {
+      multiplied.push_back(scalar * val);
+    }
+    return NVector<T>(multiplied);
+  }
+  NVector operator*(const NVector<T> &other) const {
+    std::vector<T> multiplied;
+    const std::vector<T> &otherCoords = other.GetCoords();
+    for (size_t i = 0; i < m_coords.size(); i++) {
+      multiplied.push_back(m_coords[i] * otherCoords[i]);
+    }
+    return NVector<T>(multiplied);
+  }
+  friend NVector operator*(const NVector &vec, float scalar) {
+    return scalar * vec; // Reuse previous function
+  }
+  friend NVector operator+(const NVector &v1, const NVector &v2) {
+    if (v1.m_coords.size() != v2.m_coords.size()) {
+      throw std::runtime_error("Vector sizes must match for addition");
+    }
+    std::vector<T> sum;
+    for (size_t i = 0; i < v1.m_coords.size(); i++) {
+      sum.push_back(v1.m_coords[i] + v2.m_coords[i]);
+    }
+    return NVector<T>(sum);
+  }
+
+  str toString() const {
+    str components = "";
+    for (T item : m_coords) {
+      components += std::to_string(item) + " ";
+    }
+    return components;
+  }
 };
